30d7cab956c643d178471b83c00bad8f0380024e
Patcharapon Tappakwan <patcharapon.tap@dome.tu.ac.th>
2024-07-24 19:02:01 +0700

    Add confetti audio

--- pond-kridsada/js/script.js
+++ pond-kridsada/js/script.js
@@ -1,9 +1,14 @@
+const confetti_audio = new Audio('audio/Voicy_Confetti Sound effect.mp3');
+const element = document.getElementById('e0DQ82qcIov1');
+
 function confettiShooter() {
-    const element = document.getElementById('e0DQ82qcIov1');
     element.svgatorPlayer.ready(function () {
         // this refers to the player object
-        const player = element ? element.svgatorPlayer : {};
-        if (player.play) {
+        const player = element.svgatorPlayer;
+        const audio = confetti_audio;
+        console.log(player.isPlaying);
+        if (!player.isPlaying) {
+            audio.play();
             player.play();
         }
     });
